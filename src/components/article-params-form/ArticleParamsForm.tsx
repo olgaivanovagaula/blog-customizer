5deb96dd91350a515b2909ea31279bbfd64ca2dd
olgaivanovagaula <olgaivanova2512@yandex.ru>
2025-05-16 09:23:28 +0300

    first review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,117 @@
-import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
+import { ArrowButton } from '../../ui/arrow-button';
+import { Button } from '../../ui/button';
+import { Text } from '../../ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+import { Select } from '../../ui/select';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../../ui/radio-group';
+import { Separator } from '../../ui/separator';
+import { useOutsideClickClose } from '../../ui/select/hooks/useOutsideClickClose';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	stateForm: (formState: typeof defaultArticleState) => void;
+};
+
+export const ArticleParamsForm = ({ stateForm }: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const rootRef = useRef<HTMLDivElement>(null);
+	const [selectArticleState, setSelectArticleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
+		setSelectArticleState({ ...selectArticleState, [key]: value });
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+	});
+
+	console.log(selectArticleState);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton onClick={() => setIsOpen(true)} isOpen={isOpen} />
+			<div ref={rootRef} className={clsx(styles.container__width)}>
+				<aside
+					className={clsx(styles.container, isOpen && styles.container_open)}>
+					<form
+						className={clsx(styles.form, isOpen && styles.container_relative)}
+						onSubmit={(e) => {
+							e.preventDefault();
+							stateForm(selectArticleState);
+						}}
+						onReset={(e) => {
+							e.preventDefault();
+							stateForm(defaultArticleState);
+							setSelectArticleState(defaultArticleState);
+						}}>
+						<Text weight={800} uppercase as={'h3'} size={31}>
+							Задайте параметры
+						</Text>
+						<Select
+							title='Шрифт'
+							selected={selectArticleState.fontFamilyOption}
+							options={fontFamilyOptions}
+							onChange={(option) => {
+								handleChange('fontFamilyOption', option);
+							}}
+						/> 
+						<RadioGroup
+							title={'Размер шрифта'}
+							name={'Размер шрифта'}
+							selected={selectArticleState.fontSizeOption}
+							options={fontSizeOptions}
+							onChange={(option) => {
+								handleChange('fontSizeOption', option);
+							}}
+						/>
+						<Select
+							title='цвет шрифта'
+							selected={selectArticleState.fontColor}
+							options={fontColors}
+							onChange={(option) => {
+								handleChange('fontColor', option);
+							}}
+						/>
+						<Separator />
+						<Select
+							title='цвет фона'
+							selected={selectArticleState.backgroundColor}
+							options={backgroundColors}
+							onChange={(option) => {
+								handleChange('backgroundColor', option);
+							}}
+						/>
+						<Select
+							title='ширина контента'
+							selected={selectArticleState.contentWidth}
+							options={contentWidthArr}
+							onChange={(option) => {
+								handleChange('contentWidth', option);
+							}}
+						/>
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' type='clear' htmlType='reset' />
+							<Button title='Применить' type='apply' htmlType='submit' />
+						</div>
+					</form>
+				</aside>
+			</div>
 		</>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,34 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { useState, StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { ArticleStateType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import './styles/index.module.scss'; 
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [currentState, setCurrentState] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<main
-			className={clsx(styles.main)}
+			className="main"
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': currentState.fontFamilyOption.value,
+					'--font-size': currentState.fontSizeOption.value,
+					'--font-color': currentState.fontColor.value,
+					'--container-width': currentState.contentWidth.value,
+					'--bg-color': currentState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm stateForm={setCurrentState} />
 			<Article />
 		</main>
 	);
